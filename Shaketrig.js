8fbcb2186f2e9b3585e5705a68112be68789b1a0
Nj1995 <njrealubitpriv@gmail.com>
2016-07-22 19:05:22 +0800

    Add files via upload

---
+++ Shaketrig.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+private var camerashake : CameraShake;
+public var audio1 : AudioClip;
+var audio2: AudioSource;
+var debris : Transform;
+var spawnDebri : Transform;
+
+
+function Start () 
+{
+	camerashake = GameObject.Find("MainCamera").GetComponent(CameraShake);
+	audio2 = GetComponent.<AudioSource>();
+
+}
+
+function OnMouseDown ()
+{
+	{
+	    
+	    GetComponent.<AudioSource>().PlayOneShot(audio1, 1);
+	    Instantiate(debris, spawnDebri.transform.position, spawnDebri.transform.rotation);
+		camerashake.CameraShake();
+		yield WaitForSeconds(13);
+		Application.LoadLevel("AfterEQ");
+	}
+ 
+}
---
+++ spriteActivate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteActivate0 : MonoBehaviour {
+	public GameObject spriteActive;
+	public bool isInactive;
+	public bool isActive;
+	public AudioClip Audio;
+
+
+
+	// Use this for initialization
+	void Start () {
+		spriteActive.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnButtonDown(){
+		
+		if(isInactive){
+			spriteActive.gameObject.SetActive (true);
+
+		}
+		if(isActive){
+			spriteActive.gameObject.SetActive (false);
+		}
+	}
+}
