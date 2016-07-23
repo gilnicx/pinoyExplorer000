3fe27bc815e7cc3aa34156eff50ec975718f19c5
Nj1995 <njrealubitpriv@gmail.com>
2016-07-23 14:23:32 +0800

    Add files via upload

---
+++ Scenes/PNG/spriteActivate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteActivate : MonoBehaviour {
+	public GameObject spriteActive;
+	public bool isIncative;
+	public bool isActive;
+
+
+	// Use this for initialization
+	void Start () {
+		spriteActive.gameObject.SetActive (false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown(){
+		if(isIncative){
+			spriteActive.gameObject.SetActive (true);
+		}
+		if(isActive){
+			spriteActive.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Scenes/sceneSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceneSelect : MonoBehaviour {
+
+	public bool is1 = false;
+	public bool is2 = false;
+	public bool is3 = false;
+	public bool is4 = false;
+	public bool isQuit = false;
+
+	void OnMouseDown(){
+		if (is1){
+			Application.LoadLevel ("1");
+		}
+		if (is2){
+			Application.LoadLevel ("2");
+		}
+		if (is3){
+			Application.LoadLevel ("3");
+		}
+		if (is4){
+			Application.LoadLevel ("0");
+		}
+		if (isQuit) {
+			Application.Quit ();
+		}
+	}
+}
